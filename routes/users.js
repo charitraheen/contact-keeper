b904d1355d37b6cb3e4b5d7a1701deb6dcf41142
shubhampatrick <shubhampaitrik8@gmail.com>
2020-04-06 09:43:36 +0530

    User Model & Validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validatorResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -7,8 +8,23 @@ const User = require("../models/User");
 // @desc    Register in user
 // @access  Public
 
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validatorResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
